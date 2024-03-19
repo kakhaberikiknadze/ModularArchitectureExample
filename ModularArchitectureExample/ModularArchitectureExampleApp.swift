80781e510a1dc25a9ccf28e6fce26f7bce067ad8
Kakhi <kakhaberi.kiknadze.1@gmail.com>
2024-03-19 21:02:10 +0400

    Initial Commit

---
+++ ModularArchitectureExample/ModularArchitectureExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularArchitectureExampleApp.swift
+//  ModularArchitectureExample
+//
+//  Created by Kakhi on 19.03.24.
+//
+
+import SwiftUI
+
+@main
+struct ModularArchitectureExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
